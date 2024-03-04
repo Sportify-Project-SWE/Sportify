b4401d24091c60943aa70b21a0f5bf8e74701454
anju <anjusoma@gmail.com>
2024-03-04 10:19:37 -0600

    again

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: './Sportify', // Output directory set to the root
-  } 
+  plugins: [react()]
 })
