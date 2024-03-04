5b18252972ee445d9e996bec2a722e8905e077ef
anju <anjusoma@gmail.com>
2024-03-04 10:12:57 -0600

    vite config

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: './', // Output directory set to the root
+    outDir: '../', // Output directory set to the root
   } 
 })
